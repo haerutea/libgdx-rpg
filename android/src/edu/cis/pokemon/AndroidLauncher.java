b9e50e8d95136fb46b3e9bb0b41a05a88cc7d6b9
haerutea <tszya2020@student.cis.edu.hk>
2020-05-18 20:33:02 +0800

    adding setup files

---
+++ android/src/edu/cis/pokemon/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package edu.cis.pokemon;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import edu.cis.pokemon.Pokemon;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Pokemon(), config);
+	}
+}
---
+++ desktop/src/edu/cis/pokemon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.cis.pokemon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.cis.pokemon.Pokemon;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pokemon(), config);
+	}
+}
