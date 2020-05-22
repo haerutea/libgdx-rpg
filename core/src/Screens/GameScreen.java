7f67f5d02ae30be4249113c61b0e8e9b1e8c0139
Allyson Benecia Chan <allysonbc2020@student.cis.edu.hk>
2020-05-22 18:49:42 +0800

    tiled stuff, doesn't work

--- core/src/Screens/GameScreen.java
+++ core/src/Screens/GameScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -19,11 +22,35 @@ public class GameScreen implements Screen {
     private Viewport gamePort;
     private Hud hud;
 
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
     public GameScreen(Pokemon game) {
         this.game = game;
         gameCam = new OrthographicCamera();
         gamePort = new FitViewport(PKMConstants.V_WIDTH, PKMConstants.V_HEIGHT, gameCam);
         hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+//        map = mapLoader.load("map.tmx");
+//        renderer = new OrthogonalTiledMapRenderer(map);
+
+        gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+    }
+
+    public void handleInput(float deltaTime) {
+        if(Gdx.input.isTouched()) {
+            gameCam.position.x += 100 * deltaTime; //TODO: temp check
+        }
+    }
+
+    public void update(float deltaTime) {
+        handleInput(deltaTime);
+        gameCam.update();
+
+//        renderer.setView(gameCam);
+
     }
 
     @Override
@@ -32,9 +59,14 @@ public class GameScreen implements Screen {
     }
 
     @Override
-    public void render(float delta) {
+    public void render(float deltaTime) {
+        update(deltaTime);
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+//		renderer.render();
+
 		game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
 		hud.stage.draw();
     }
