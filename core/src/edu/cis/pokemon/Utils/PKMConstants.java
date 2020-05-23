ad73d34cb79bde682e8000c6a6cc841e0931661c
haerutea <tszya2020@student.cis.edu.hk>
2020-05-23 18:55:11 +0800

    added atlas and menu image filename

--- core/src/edu/cis/pokemon/Utils/PKMConstants.java
+++ core/src/edu/cis/pokemon/Utils/PKMConstants.java
@@ -5,4 +5,13 @@ public class PKMConstants {
     public static final int V_HEIGHT = 500; //arbitrary numbers
     public static final int V_WIDTH = 500;
 
+    //atlas file name
+    public static final String ATLAS_FILENAME = "player.atlas";
+
+    //menu image file name
+    public static final String MENU_FILENAME = "badlogic.jpg";
+    //animation sprites
+    public static final String PLAYER_SPRITE = "trainers";
+
+
 }
