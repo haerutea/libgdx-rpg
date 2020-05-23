1c4ce38458f6e8107f38c73ee190d66dfdd6717d
haerutea <tszya2020@student.cis.edu.hk>
2020-05-23 16:40:05 +0800

    fixed map

--- android/assets/pokemon_town.tsx
+++ android/assets/pokemon_town.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.4" name="pokemon_town" tilewidth="16" tileheight="16" margin="3" tilecount="2120" columns="40">
- <image source="tiles/town.png" width="652" height="861"/>
+ <image source="town.png" width="652" height="861"/>
 </tileset>
--- android/assets/various_tiles.tsx
+++ android/assets/various_tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.4" name="various_tiles" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="480" columns="20">
- <image source="tiles/various_tiles.png" width="343" height="410"/>
+ <image source="various_tiles.png" width="343" height="410"/>
 </tileset>
