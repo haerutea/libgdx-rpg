943f19f08aff833b49421dabe934624180b2153a
haerutea <tszya2020@student.cis.edu.hk>
2020-05-25 20:35:02 +0800

    added layer indexes, added box2d graphics, added collision bits (they all start with BIT so it's easier to 'search')

--- core/src/edu/cis/pokemon/Utils/PKMConstants.java
+++ core/src/edu/cis/pokemon/Utils/PKMConstants.java
@@ -10,10 +10,31 @@ public class PKMConstants {
     //map file name
     public static final String MAP_FILENAME = "new_town.tmx";
 
+    //map layer indexes
+    public static final int ENVIRONMENT = 0;
+    public static final int GRASS = 1;
+    public static final int LEDGES = 2;
+    public static final int DOORS = 3;
+    public static final int SIGNS = 4;
+    public static final int ITEMS = 5;
+    public static final int TRAINERS = 6;
+
     //menu image file name
     public static final String MENU_FILENAME = "badlogic.jpg";
     //animation sprites
     public static final String PLAYER_SPRITE = "trainers";
 
+    //graphics
+    public static final float FPS = 1/60f;
+    public static final int V_ITER = 6;
+    public static final int P_ITER = 2;
 
+    //collision bits
+    public static final short BIT_PLAYER = 1;
+    public static final short BIT_IMPASSIBLE = 2;
+    public static final short BIT_SIGN = 4;
+    public static final short BIT_LEDGE = 8;
+    public static final short BIT_GRASS = 16;
+    public static final short BIT_ITEM = 32;
+    public static final short BIT_DOOR = 64;
 }
