422b2797553d1e1cff2c337521fe227ef4f617a3
haerutea <tszya2020@student.cis.edu.hk>
2020-05-25 21:58:35 +0800

    added utils to create fixtures

--- core/src/edu/cis/pokemon/Utils/PKMUtils.java
+++ core/src/edu/cis/pokemon/Utils/PKMUtils.java
@@ -1,4 +1,22 @@
 package edu.cis.pokemon.Utils;
 
-public class PKMUtils {
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+
+public class PKMUtils
+{
+    public static FixtureDef createGameFixture(Object userData, Body box2Body, short fixtureBit, short canCollideWith)
+    {
+        FixtureDef fixtureDef = new FixtureDef();
+        CircleShape circleShape = new CircleShape();
+        circleShape.setRadius(6);
+        fixtureDef.filter.categoryBits = fixtureBit;
+        fixtureDef.filter.maskBits = canCollideWith;
+
+        fixtureDef.shape = circleShape;
+        box2Body.createFixture(fixtureDef).setUserData(userData);
+
+        return fixtureDef;
+    }
 }
