788433840628927965622c739dba5a98b7437f8f
haerutea <tszya2020@student.cis.edu.hk>
2020-05-25 21:55:48 +0800

    added player movement

--- core/src/edu/cis/pokemon/Sprites/Player.java
+++ core/src/edu/cis/pokemon/Sprites/Player.java
@@ -1,6 +1,6 @@
 package edu.cis.pokemon.Sprites;
 
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -8,22 +8,17 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.Array;
 
+import edu.cis.pokemon.Enums.Direction;
+import edu.cis.pokemon.Enums.State;
 import edu.cis.pokemon.Screens.GameScreen;
 import edu.cis.pokemon.Utils.PKMConstants;
+import edu.cis.pokemon.Utils.PKMUtils;
 
 public class Player extends Sprite
 {
-    public enum State {
-        STANDING, WALKING
-    }
-    public enum Direction {
-        FRONT, BACK, LEFT, RIGHT
-    }
     private State currentState;
     private Direction currentDirection;
 
@@ -49,13 +44,35 @@ public class Player extends Sprite
 
         playerRegion = screen.getAtlas().findRegion(PKMConstants.PLAYER_SPRITE);
         makeAnimations();
+        defineBox2d();
 
+        setBounds(0, 0, 16, 16);
+        setRegion(frontStand);
+    }
 
+    public void defineBox2d()
+    {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.position.set(150, 150);
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        box2Body = world.createBody(bodyDef);
+
+        //TODO: CHANGE THIS SO IT CAN WALK THROUGH THE GRASS
+        short collidesWith =
+                PKMConstants.BIT_IMPASSIBLE
+                | PKMConstants.BIT_SIGN
+                | PKMConstants.BIT_LEDGE
+                | PKMConstants.BIT_ITEM
+                | PKMConstants.BIT_GRASS
+                | PKMConstants.BIT_DOOR;
+
+        FixtureDef fixtureDef = PKMUtils.createGameFixture(this, box2Body, PKMConstants.BIT_PLAYER, collidesWith);
+        setRegion(frontStand);
     }
 
     private void makeAnimations()
     {
-        frontStand = new TextureRegion(playerRegion, 0, 0, 16, 16);
+        frontStand = new TextureRegion(playerRegion, 2, 2, 16, 16);
         backStand = new TextureRegion(playerRegion, 16, 0, 16, 16);
         sideStand = new TextureRegion(playerRegion, 32, 0, 16, 16);
 
@@ -63,5 +80,43 @@ public class Player extends Sprite
 //        frames.add(new TextureRegion(playerRegion, i * 16, 0, 16, 16));
 //        frontWalk = new Animation<>(0.1f, frames);
 //        frames.clear();
+    }
+
+    public void changeSprite(Direction direction, State state)
+    {
+        Gdx.app.log("changeSprite", "works");
+        currentDirection = direction;
+        currentState = state;
+        switch(direction)
+        {
+            case FRONT:
+                Gdx.app.log("front", "works");
+                setRegion(frontStand);
+                break;
+            case BACK:
+                setRegion(backStand);
+                break;
+        }
+    }
+
+    public void move()
+    {
+        if(currentState != State.STANDING)
+        {
+            if(currentDirection.getAxis().equals("x"))
+            {
+                box2Body.applyLinearImpulse(new Vector2(currentDirection.getVelocity(), 0), box2Body.getWorldCenter(), true);
+            }
+            else if(currentDirection.getAxis().equals("y"))
+            {
+                box2Body.applyLinearImpulse(new Vector2(0, currentDirection.getVelocity()), box2Body.getWorldCenter(), true);
+            }
+        }
+    }
+
+    public void stop()
+    {
+        currentState = State.STANDING;
+        box2Body.setLinearVelocity(new Vector2(0, 0));
     }
 }
