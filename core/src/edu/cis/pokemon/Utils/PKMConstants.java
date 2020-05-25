fe0622eac973bdecda18f815ee38e504a44868ac
haerutea <tszya2020@student.cis.edu.hk>
2020-05-26 02:39:18 +0800

    fixed map layer indexes, added x/y offset for spritesheet

--- core/src/edu/cis/pokemon/Utils/PKMConstants.java
+++ core/src/edu/cis/pokemon/Utils/PKMConstants.java
@@ -6,23 +6,26 @@ public class PKMConstants {
     public static final int V_WIDTH = 11 * 16;
 
     //atlas file name
-    public static final String ATLAS_FILENAME = "player.atlas";
+    public static final String ATLAS_FILENAME = "sprites.atlas";
     //map file name
     public static final String MAP_FILENAME = "new_town.tmx";
 
     //map layer indexes
-    public static final int ENVIRONMENT = 0;
-    public static final int GRASS = 1;
-    public static final int LEDGES = 2;
-    public static final int DOORS = 3;
-    public static final int SIGNS = 4;
-    public static final int ITEMS = 5;
-    public static final int TRAINERS = 6;
+    public static final int ENVIRONMENT = 1;
+    public static final int GRASS = 2;
+    public static final int LEDGES = 3;
+    public static final int DOORS = 4;
+    public static final int SIGNS = 5;
+    public static final int ITEMS = 6;
+    public static final int TRAINERS = 7;
 
     //menu image file name
     public static final String MENU_FILENAME = "badlogic.jpg";
     //animation sprites
     public static final String PLAYER_SPRITE = "trainers";
+    //x and y offset for sprites
+    public static final int X_OFFSET = 4;
+    public static final int Y_OFFSET = 4;
 
     //graphics
     public static final float FPS = 1/60f;
@@ -37,4 +40,5 @@ public class PKMConstants {
     public static final short BIT_GRASS = 16;
     public static final short BIT_ITEM = 32;
     public static final short BIT_DOOR = 64;
+    public static final short BIT_TRAINER = 128;
 }
