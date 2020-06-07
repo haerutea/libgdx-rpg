49c087bca4f7410f11e3d99642280b856849a1a8
haerutea <tszya2020@student.cis.edu.hk>
2020-06-07 13:31:35 +0800

    added black border

---
+++ android/assets/black_tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="black_tile" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="black_tile.png" width="16" height="16"/>
+</tileset>
